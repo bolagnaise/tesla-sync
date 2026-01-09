6055e8109985524d8180595a0219a8c81af131cc
bolagnaise <bolagnaise@msn.com>
2026-01-09 20:13:16 +1000

    Fix migration for fronius_load_following - unique revision ID

--- migrations/versions/b1u2v3w4x5y6_add_fronius_load_following.py
+++ migrations/versions/b1u2v3w4x5y6_add_fronius_load_following.py
@@ -1,8 +1,8 @@
 """Add fronius_load_following column
 
-Revision ID: a0t1u2v3w4x5
-Revises: z9s0t1u2v3w4
-Create Date: 2026-01-09 20:10:00.000000
+Revision ID: b1u2v3w4x5y6
+Revises: a0t1u2v3w4x5
+Create Date: 2026-01-09 20:15:00.000000
 
 """
 from alembic import op
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a0t1u2v3w4x5'
-down_revision = 'z9s0t1u2v3w4'
+revision = 'b1u2v3w4x5y6'
+down_revision = 'a0t1u2v3w4x5'
 branch_labels = None
 depends_on = None
 
@@ -19,6 +19,9 @@ depends_on = None
 def upgrade():
     with op.batch_alter_table('user', schema=None) as batch_op:
         batch_op.add_column(sa.Column('fronius_load_following', sa.Boolean(), nullable=True, default=False))
+
+    # Set default value for existing rows
+    op.execute("UPDATE user SET fronius_load_following = 0 WHERE fronius_load_following IS NULL")
 
 
 def downgrade():
